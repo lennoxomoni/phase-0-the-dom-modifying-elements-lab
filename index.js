52eeb05f8346a4ab0c60137b7a1d19eeaa7afc32
lennox <lennoxngubo549@gmail.com>
2024-10-09 14:54:50 +0300

    phase-0-the-dom-modifying-elements-lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+console.log("Script is running");
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory'
+newHeader.textContent = "Your Header Text";
+newHeader.textContent = "Your name is the champion"
+document.body.appendChild(newHeader);
+console.log(newHeader);
